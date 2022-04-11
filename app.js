5dc6923b95aab4d0dace2f554eea4452577d5a7f
Jeffrey <jeffreyhu16@gmail.com>
2022-04-11 10:23:07 +0800

    change port message

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 })
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Listening on port 5000'));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
